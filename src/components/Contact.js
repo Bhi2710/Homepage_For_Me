f6e26b634a122511c8c19ce7ebb2770349e1894f
Bhi2710 <92436824+Bhi2710@users.noreply.github.com>
2022-09-02 11:31:24 +0530

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,9 +2,9 @@ const Contact = _ => {
     return(
         <section>
             <h2>Contact ME:</h2>
-            <a href="https://www.linkedin.com/in/abhishek-yadav-1606aa18b/" target="_blank">LinkedIn</a>
-            <a href="https://github.com/Bhi2710" target="_blank">Github</a>
-            <a href="https://www.instagram.com/ab_his_h_ek/" target="_blank">Intagram</a>
+            <a href="https://www.linkedin.com/in/abhishek-yadav-1606aa18b/" target="_blank" rel="noreferrer">LinkedIn</a>
+            <a href="https://github.com/Bhi2710" target="_blank" rel="noreferrer">Github</a>
+            <a href="https://www.instagram.com/ab_his_h_ek/" target="_blank" rel="noreferrer">Intagram</a>
         </section>    
     )
 }
